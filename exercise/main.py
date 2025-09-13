3095e9e2f08f95c540361eaac9996af15dd00dfc
martywebdev <martywebdevelopment@gmail.com>
2025-09-13 10:32:32 +0800

    completed exercise

--- exercise/main.py
+++ exercise/main.py
@@ -1,26 +1,35 @@
 import pandas as pd
 from fpdf import FPDF
 
-from pdf_code import create_pdf
+from pdf_code import Receipt
 
 
 df = pd.read_csv('articles.csv')
 
 
 class Article:
-    def __init__(self, article_id, df, pdf_func=create_pdf):
+    def __init__(self, article_id, df, pdf_cls=Receipt):
         # find the article in df
+        self.df = df  # keep reference
         result = df.loc[df['id'] == int(article_id)]
         if result.empty:
             raise ValueError(f"No article found with id={article_id}")
         self.data = result.iloc[0]
-
+        self.index = result.index[0]
         # store PDF function
-        self.pdf_func = pdf_func
+        self.pdf_cls = pdf_cls
+
+    def available(self):
+        return self.data['in stock'] > 0
+
+    def sales(self):
+        self.df.at[self.index, 'in stock'] -= 1
+        self.df.to_csv('articles.csv', index=False)
 
     def print_pdf(self):
         # use the injected function
-        self.pdf_func(self.data)
+        receipt = self.pdf_cls(self.data)  # instantiate class
+        receipt.generate()   
 
 
 while True:
@@ -30,6 +39,8 @@ while True:
         if user_input == 'q':
             break
         article = Article(user_input, df)
-        article.print_pdf()
-    except (IndexError) as e:
+        if article.available():
+            article.sales()
+            article.print_pdf()
+    except (IndexError, ValueError) as e:
         print(e)
--- exercise/pdf_code.py
+++ exercise/pdf_code.py
@@ -1,16 +1,20 @@
 from fpdf import FPDF
 
-def create_pdf(article):
-    pdf = FPDF(orientation="P", unit="mm", format="A4")
-    pdf.add_page()
+class Receipt:
+    def __init__(self, article):
+        self.article = article
+        
+    def generate(self):
+        pdf = FPDF(orientation="P", unit="mm", format="A4")
+        pdf.add_page()
 
-    pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Receipt nr.{article['id']}", ln=1)
+        pdf.set_font(family="Times", size=16, style="B")
+        pdf.cell(w=50, h=8, txt=f"Receipt nr.{self.article['id']}", ln=1)
 
-    pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Article: {article['name']}", ln=1)
+        pdf.set_font(family="Times", size=16, style="B")
+        pdf.cell(w=50, h=8, txt=f"Article: {self.article['name']}", ln=1)
 
-    pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Price: {article['price']}", ln=1)
+        pdf.set_font(family="Times", size=16, style="B")
+        pdf.cell(w=50, h=8, txt=f"Price: {self.article['price']}", ln=1)
 
-    pdf.output("receipt.pdf")
+        pdf.output("receipt.pdf")
